a56d1728adb1fc219c322b5b0ce09bbae1be5aed
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-08-29 20:22:33 -0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-### SOBRESCREVER ARQUIVO "mirrorlist" EM "/etc/pacman.d"
 echo "Server=https://mirror.ufscar.br/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist;
 
 
-
-
-
-### SOBRESCREVER ARQUIVO "pacman.conf" EM "/etc"
 echo "[options]
 Architecture=auto
 CheckSpace
@@ -24,17 +19,9 @@ Include=/etc/pacman.d/mirrorlist
 Include=/etc/pacman.d/mirrorlist" > /etc/pacman.conf;
 
 
-
-
-
-### SINCRONIZAR ESPELHO E ATUALIZAR PACOTES
 pacman -Syyu --noconfirm --quiet;
 
 
-
-
-
-### EXCLUIR TODOS OS DADOS, FORMATAR E CRIAR PARTIÇÕES EM APENAS 1 DOS DISPOSITIVOS DE ARMAZENAMENTO VÁLIDO
 if fdisk /dev/nvme0n1; then <<EOF
 o
 w
@@ -119,10 +106,6 @@ mount /dev/sda3 /mnt/home;
 fi;
 
 
-
-
-
-### INSTALAR BASE, DRIVERS, INTERFACE GRÁFICA E PACOTES NECESSARIOS EM "mnt"
 pacstrap /mnt --noconfirm \
 base \
 base-devel \
@@ -159,43 +142,19 @@ grub-efi-x86_64 \
 efibootmgr;
 
 
-
-
-
-### INDICAR E ANEXAR PARTIÇÕES DO SISTEMA NO ARQUIVO "fstab" EM "/etc"
 genfstab -U -p /mnt > /mnt/etc/fstab;
 
 
-
-
-
-### MOVER ARQUIVO "2.sh" PARA "/mnt"
 mv 4RC*/2.sh /mnt;
 
 
-
-
-
-### ATIVAR TODAS AS PERMISSÕES DO ARQUIVO "2.sh"
 chmod 777 /mnt/2.sh;
 
 
-
-
-
-### EXECUTAR ARQUIVO "2.sh" NO AMBIENTE "arch-chroot" EM "/mnt"
 arch-chroot /mnt ./2.sh;
 
 
-
-
-
-### SINCRONIZAR SISTEMAS DE ARQUIVOS EM MEMÓRIA COM DISPOSITIVO DE ARMAZENAMENTO
 sync;
 
 
-
-
-
-### REINICIAR O DISPOSITIVO FORÇADAMENTE
 reboot -f;
