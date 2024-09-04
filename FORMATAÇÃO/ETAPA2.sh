08fc6cb76ab40c921bce203ae2dca0a1e933a2a3
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-09-04 11:06:52 -0300

    Update ETAPA2.sh

--- FORMATAÇÃO/ETAPA2.sh
+++ FORMATAÇÃO/ETAPA2.sh
@@ -131,20 +131,6 @@ grub-mkconfig -o /boot/grub/grub.cfg;
 
 echo "4RCH ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers;
 
-sed -i "/^\s*#/d; /^\s*$/d" \
-/home/4RCH/.bash_profile \
-/home/4RCH/.bash_logout \
-/etc/sudoers \
-/etc/sudo.conf \
-/etc/host.conf \
-/etc/healthd.conf \
-/etc/mkinitcpio.conf \
-/etc/libva.conf \
-/etc/vconsole.conf \
-/etc/fuse.conf \
-/etc/ts.conf \
-/etc/fstab;
-
 sed -i "/^UUID=.* \/boot .*$/! s/rw/rw,noatime,discard,/" /etc/fstab;
 
 echo "127.0.0.1 localhost.localdomain localhost
