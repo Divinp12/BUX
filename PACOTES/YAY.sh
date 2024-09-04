3f92913065a580006d23293b6d3964874919ec30
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-09-04 11:03:19 -0300

    Delete PACOTES directory

--- PACOTES/YAY.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://aur.archlinux.org/yay.git && \
-sudo chmod 777 yay && \
-cd yay && \
-makepkg -si --noconfirm && \
-cd .. && \
-sudo rm -rf yay && \
-yay -S --noconfirm nano --save --answerdiff None --answerclean None --removemake;
