c4b12322cea27c49ec673f71016b3161ed2c3681
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-09-04 13:53:33 -0300

    Update YAY.sh

--- FORMATAÇÃO/PACOTES/YAY.sh
+++ FORMATAÇÃO/PACOTES/YAY.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 git clone https://aur.archlinux.org/yay.git /home/4RCH && \
-chmod 777 yay;
+chmod 777 yay && \
+cd yay && \
+makepkg -si --noconfirm && \
+cd .. && \
+sudo rm -rf yay && \
+yay -S --noconfirm nano --save --answerdiff None --answerclean None --removemake;
