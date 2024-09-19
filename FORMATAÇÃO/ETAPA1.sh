8640f5ed3a87d2b802c390c58c956cf26a49660e
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-09-19 20:36:41 +0000

    Update ETAPA1.sh

--- FORMATAÇÃO/ETAPA1.sh
+++ FORMATAÇÃO/ETAPA1.sh
@@ -286,6 +286,35 @@ arch-chroot /mnt bash -c 'grub-install --target=x86_64-efi --efi-directory=/boot
 arch-chroot /mnt bash -c 'grub-mkconfig -o /boot/grub/grub.cfg';
 
 
+echo "4RCH ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers;
+
+
+sed -i "/^UUID=.* \/boot .*$/! s/rw/rw,noatime,discard,/" /etc/fstab;
+
+
+echo "127.0.0.1 localhost.localdomain localhost
+::1 localhost.localdomain localhost
+127.0.0.1 4RCH.localdomain 4RCH" > /etc/hosts;
+
+
+sed -i "/^\s*#/d; /^\s*$/d" \
+/home/4RCH/.bash_profile \
+/home/4RCH/.bash_logout \
+/etc/sudoers \
+/etc/sudo.conf \
+/etc/host.conf \
+/etc/healthd.conf \
+/etc/mkinitcpio.conf \
+/etc/libva.conf \
+/etc/vconsole.conf \
+/etc/fuse.conf \
+/etc/ts.conf \
+/etc/fstab;
+
+
+rm -rf /boot/initramfs-linux-fallback.img;
+
+
 arch-chroot /mnt ./ETAPA2.sh;
 
 
