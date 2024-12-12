a12246c2da4654b238042ef8a884dadde786908d
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-12-12 18:50:13 -0300

    Update AUTOINSTALACAO.sh

--- AUTOINSTALACAO.sh
+++ AUTOINSTALACAO.sh
@@ -162,9 +162,12 @@ S
 if X pacstrap /mnt --noconfirm --quiet \
 base \
 base-devel \
+linux \
 linux-firmware \
 linux-headers \
 networkmanager \
+tar \
+wget \
 sudo \
 git \
 fastfetch \
@@ -216,6 +219,13 @@ clear; "$@"
 X() {
 "$@" > /dev/null 2>&1;
 }
+
+S
+
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.12.4.tar.xz && \
+tar -xvf linux-6.12* && \
+cd linux-6* && \
+make localmodconfig && \
 
 S
 
