0287323e8ee5846d92842d8c97f3c82010fd1ac0
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-12-27 20:21:17 +0000

    Update AUTOINSTALACAO.sh

--- AUTOINSTALACAO.sh
+++ AUTOINSTALACAO.sh
@@ -2,12 +2,14 @@
 
 clear;
 
+######################################################################################################################################
 if echo "INICIANDO FORMATACAO EM 10 SEGUNDOS,
 ESTEJA CIENTE DO QUE ESTA FAZENDO,
 POIS TODOS OS DADOS SERAO APAGADOS
 PARA QUE A INSTALACAO SEJA BEM SUCEDIDA..."; then
 sleep 10; clear
 fi;
+######################################################################################################################################
 
 echo "ADICIONANDO ESPELHO BRASILEIRO"
 if echo "Server=https://mirror.ufscar.br/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist; then
