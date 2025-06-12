cc249f1d1902265298368a2bd84e915826eb6968
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-06-11 21:20:50 -0300

    Update AUTOINSTALACAO.sh

--- AUTOINSTALACAO.sh
+++ AUTOINSTALACAO.sh
@@ -264,6 +264,10 @@ sed -i 's/^.*CONFIG_F2FS_FS.*$/CONFIG_F2FS_FS=n/' .config;
 sed -i 's/^.*CONFIG_BCACHEFS_FS.*$/CONFIG_BCACHEFS_FS=n/' .config;
 sed -i 's/^.*CONFIG_NTFS_FS.*$/CONFIG_NTFS_FS=n/' .config;
 
+make -j$(nproc);
+make modules_install;
+make install;
+
 
 
 sleep 3;
