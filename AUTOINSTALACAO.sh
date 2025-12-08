558f47f11be76103a58feb521ddce80f4c4d3567
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2025-12-08 13:37:56 -0300

    Update AUTOINSTALACAO.sh

--- AUTOINSTALACAO.sh
+++ AUTOINSTALACAO.sh
@@ -321,7 +321,7 @@ echo "sobscrevendo arquivo grub";
 if echo "GRUB_DEFAULT=0
 GRUB_TIMEOUT=0
 GRUB_DISTRIBUTOR=\"bux\"
-GRUB_CMDLINE_LINUX_DEFAULT=\"quiet loglevel=0 panic=0 mitigations=off\"
+GRUB_CMDLINE_LINUX_DEFAULT=\"quiet loglevel=0 mitigations=off panic=0 nowatchdog nmi_watchdog=0 softlockup_panic=0 hardlockup_panic=0\"
 GRUB_CMDLINE_LINUX=\"\"
 GRUB_PRELOAD_MODULES=\"part_gpt part_msdos\"
 GRUB_GFXMODE=auto
