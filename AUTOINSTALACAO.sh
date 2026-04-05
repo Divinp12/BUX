d1cc788f9feb6f70665f6bd51d2c5722a7ad9fd6
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2026-04-05 20:47:42 -0300

    Update AUTOINSTALACAO.sh

--- AUTOINSTALACAO.sh
+++ AUTOINSTALACAO.sh
@@ -220,7 +220,7 @@ echo "criando autostartx do sway";
 echo "if [ \"\$(tty)\" = \"/dev/tty1\" ]; then
 exec sway > /dev/null 2>&1
 fi;
-export SWAYSOCK=/run/user/1000/sway-ipc*;
+export SWAYSOCK=/run/user/$(id -u)/sway-ipc.*.sock;
 alias i=\"yay -Sy --noconfirm\";
 alias d=\"sudo pacman -Rsc\";
 alias a=\"yay -Syyu --noconfirm\";
